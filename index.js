12b74eb32ddb4315683cf5c6463299bad2602398
Beefs4000 <103645633+Beefs4000@users.noreply.github.com>
2022-09-24 14:00:41 +0800

    completed

--- index.js
+++ index.js
@@ -1,13 +1,138 @@
-// TODO: Include packages needed for this application
+// required packages
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path =  require('path');
+const templateLocation = path.join(__dirname,'utils', 'template.md');
+const outputLocation = path.join(__dirname, 'output', 'README.md');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Use chalk to colour console log text
+const green = chalk.green;
+const red = chalk.red;
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Welcome user
+console.log(green('Hello, thank you for using this Read Me Generator. Please follow the prompts to create a quality .MD for your project.'));
+console.log(red('Adding code snippets or line breaks is easy, just use use backticks and <br> respectively'));
 
-// TODO: Create a function to initialize app
-function init() {}
+// List of questions
 
-// Function call to initialize app
-init();
+inquirer.prompt([
+    // Name of Project
+    {
+        type: 'input',
+        message: 'Name your Project.',
+        name:'title',
+    },
+    // Description
+    {
+        type: 'input',
+        message: 'Please describe your Project.',
+        name:'description',
+    },
+    // Modules used
+    {
+        type: 'input',
+        message: 'What did you use to build the project?.',
+        name:'built',
+    },    
+    // installation instructions
+    {
+        type: 'input',
+        message: 'How do you install the project?',
+        name:'installation',
+    },
+    // How to use the project
+    {
+        type: 'input',
+        message: 'Explain how to use the project?',
+        name:'usage',
+    },
+    // Contribution
+    {
+        type: 'input',
+        message: 'How would you like people to contribute?',
+        name:'contribution',
+    },
+    // How to test
+    {
+        type: 'input',
+        message: 'What are the project test instructions?',
+        name:'test',
+    },
+    // email
+    {
+        type: 'input',
+        message: 'Enter your email',
+        name:'email',
+    },
+    // license
+    {
+        type: 'list',
+        message: 'What is the license?',
+        name:'license',
+        choices: [
+            'MIT','GPL-3.0', 'Apache-v2', 'ISC'
+        ]
+    },
+    // github
+    {
+        type: 'input',
+        message: 'Enter your GitHub username.',
+        name:'github',
+    },
+    // project url
+    {
+        type:'input',
+        message:'Enter the GitHub project URL',
+        name: 'url'
+    },
+    // socials
+    {
+        type: 'input',
+        message: 'What is your twitter handle?',
+        name:'twitter',
+    },
+    {
+        type: 'input',
+        message: 'What is you linkedin user?',
+        name:'linkedin',
+    },
+    {
+        type: 'input',
+        message: 'Enter your name or moniker?',
+        name:'identification',
+    },
+
+
+    // render retrieved answers
+]).then((ans) => {
+    // convert the license to lowercase for URL
+    const lowerCaseLicense = ans.license.toLowerCase()
+    // Retrieves the template
+    const template = fs.readFileSync(templateLocation, 'utf-8')
+    // variable to find multiple instances of a word in the string
+    const licenses = /{{license}}/g;
+    const projectURL = /{{url}}/g
+    const gitLink = /{{github-link}}/g
+    // Replace parts of string with relevant information
+    const output = template.replace('{{title}}', ans.title)
+        .replace('{{licenseLowerCase}}', lowerCaseLicense)
+        .replace(licenses, ans.license)
+        .replace('{{description}}', ans.description)
+        .replace(projectURL, ans.url)
+        .replace('{{github}}', ans.github)
+        .replace('{{built}}', ans.built)
+        .replace('{{installation}}', ans.installation)
+        .replace('{{usage}}', ans.usage)
+        .replace('{{contribution}}',ans.contribution)
+        .replace('{{test}}',ans.test)
+        .replace(gitLink,`https://github.com/${ans.github}`)
+        .replace('{{twitter}}', `https://twitter.com/${ans.twitter}`)
+        .replace('{{email}}',`mailto:${ans.email}`)
+        .replace('{{linkedin}}', `https://www.linkedin.com/in/${ans.linkedin}/`)
+        .replace('{{identification}}', ans.identification)
+    // Turn the edited variable into a new README
+    fs.writeFileSync(outputLocation, output)
+    // Notify the user
+    console.log(green('Your README File has been exported to ' + outputLocation ))
+})
---
+++ utils/template.md
@@ -0,0 +1,77 @@
+<div id="top"></div>
+
+<center>
+
+# {{title}} <a name="title"></a><br>
+
+</center>
+
+## Description <a name="description"></a>
+
+{{description}}
+
+GitHub URL: [{{url}}]({{url}})
+
+## Table of Contents
+1. [Title](#title)
+
+2. [Built with](#Built)
+
+3. [Description](#description)
+
+4. [Installation](#installation)
+
+5. [Usage](#usage)
+
+6. [License](#license)
+
+7. [Tests](#tests)
+
+8. [Contact](#contact)
+
+
+## Built With <a name="Built"></a>
+{{built}}
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Installation <a name="installation"></a>
+{{installation}} 
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Usage <a name="usage"></a>
+{{usage}}
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## License <a name="license"></a>
+[{{license}}](https://choosealicense.com/licenses/{{licenseLowerCase}})
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Contributing <a name="contributing"></a>
+{{contribution}}
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Tests <a name="tests"></a>
+{{test}}
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Questions <a name="questions"></a>
+
+Please use the links below to contact me with any questions.
+
+{{identification}} [<img src="https://github.com/gauravghongde/social-icons/blob/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/PNG/White/Twitter_white.png" width="18">][5] [<img src="https://github.com/gauravghongde/social-icons/blob/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/PNG/White/Outlook_white.png" width="18">][6] [<img src="https://github.com/gauravghongde/social-icons/blob/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/PNG/White/Github_white.png" width="18">][7] [<img src="https://github.com/gauravghongde/social-icons/blob/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/PNG/White/LinkedIN_white.png" width="18">][8]
+
+[5]: {{twitter}}
+[6]: {{email}}
+[7]: {{github-link}}
+[8]: {{linkedin}}
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+__________________________________________________
+
+> *This README was generated using the [README Generator!](https://github.com/Beefs4000/Read-me)*
